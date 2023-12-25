79eac22b1c7e5a020a4a62ec027af0b7b550595b
DSA-Mentor <dsamentor@gmail.com>
2023-12-25 22:10:02 +0530

    Hashing 3

---
+++ Hashing/Hashing 3/Group Anagrams/Main.java
@@ -0,0 +1,81 @@
+
+// A Java program to print all anagrams together
+import java.util.*;
+
+public class Main {
+    // TC: o(M) + o(26) ~ O(M)
+    // SC: O(1)
+    static String getCode(String word) {
+        // step 1: generate a freq array
+        int[] freqArr = new int[26];
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            freqArr[(int)(ch - 'a')] += 1;
+        }
+
+        // step 2: generate encoded value from freq array
+        StringBuilder code = new StringBuilder("");
+        for (int i = 0; i < 26; i++) {
+            if (freqArr[i] > 0) {
+                char ch = (char)('a' + i);
+                code.append(ch);
+                code.append(freqArr[i]);
+            }
+        }
+
+        return code.toString();
+    }
+
+    
+    static void printAnagramsTogether(String wordArr[], int size) {
+        // Map -> key: grp code, value: grp
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+
+        ArrayList<String> firstOfEachGrp = new ArrayList<>();
+
+        for (String word : wordArr) {
+            // encode the string
+            String code = getCode(word);
+
+            if (map.containsKey(code) == true) {
+                // update the grp
+                ArrayList<String> grp = map.get(code);
+                grp.add(word);
+                map.put(code, grp);
+            } else {
+                // create a new key value pair
+                ArrayList<String> grp = new ArrayList<>();
+                grp.add(word);
+                firstOfEachGrp.add(word);
+                map.put(code, grp);
+            }
+        } 
+
+        Collections.sort(firstOfEachGrp);
+        for (String word : firstOfEachGrp) {
+            // encode the string
+            String code = getCode(word);
+
+            for (String w : map.get(code)) {
+                System.out.print(w + " ");
+            }
+        }
+
+    }
+
+    // Driver program to test above functions
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        String[] wordArr = new String[n];
+        for (int i = 0; i < n; i++) {
+            wordArr[i] = sc.next();
+        }
+    
+        printAnagramsTogether(wordArr, n);
+
+        sc.close();
+    }
+}
---
+++ Hashing/Hashing 3/Minimum Window Substring/Main.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String minWindow(String s, String t) {
+		// write code here
+        HashMap<Character, Integer> freqMap2 = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            freqMap2.put(ch, freqMap2.getOrDefault(ch, 0) + 1);
+        }
+
+        HashMap<Character, Integer> freqMap1 = new HashMap<>();
+        
+        int dmcnt = t.length();
+        int mcnt = 0;
+
+        int inc = -1;
+        int exc = -1;
+
+        String ans = "";
+
+        while (true) {
+            boolean flag1 = false;
+            if (inc + 1 < s.length() && mcnt < dmcnt) {
+                inc += 1;
+                char ch = s.charAt(inc);
+                if (freqMap2.containsKey(ch) == true) {
+                    if (freqMap2.get(ch) > freqMap1.getOrDefault(ch, 0)) {
+                        mcnt++;
+                    }
+                }
+                freqMap1.put(ch, freqMap1.getOrDefault(ch, 0) + 1);
+                flag1 = true;
+            }
+
+            boolean flag2 = false;
+            if (exc < inc && mcnt == dmcnt) {
+                if (ans == "" || ans.length() > (inc - exc)) {
+                    ans = s.substring(exc + 1, inc + 1);
+                }
+
+                exc += 1;
+                char ch = s.charAt(exc);
+                freqMap1.put(ch, freqMap1.get(ch) - 1);
+
+                if (freqMap2.containsKey(ch) == true) {
+                    if (freqMap2.get(ch) > freqMap1.get(ch)) {
+                        mcnt--;
+                    }
+                }
+
+                if (freqMap1.get(ch) == 0) {
+                    freqMap1.remove(ch);
+                }
+                flag2 = true;
+            }
+
+            if (flag1 == false && flag2 == false) {
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s,t;
+        s = sc.next();
+		t = sc.next();
+        Solution Obj = new Solution();
+        System.out.print(Obj.minWindow(s,t));
+    }
+}
