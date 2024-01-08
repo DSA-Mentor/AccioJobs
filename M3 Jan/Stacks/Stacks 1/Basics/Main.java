43124d6b0c496a387671913629f5e4bcf0d0407c
DSA-Mentor <dsamentor@gmail.com>
2024-01-08 22:06:25 +0530

    Orientaion and Stacks 1

---
+++ M3 Jan/Stacks/Stacks 1/Basics/Main.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) {
+        // intitialize a stack
+        Stack<Integer> st = new Stack<>();
+
+        // push method: Pushes an item onto the top of this stack.
+        st.push(10);
+        st.push(30);
+        st.push(40);
+        st.push(5);
+
+        // get topmost element 
+        System.out.println("topmost element: " + st.peek());
+
+        // size of the stacj
+        System.out.println("size: " + st.size());
+
+        // remove from stack
+        System.out.println("removed ele: " + st.pop());
+        System.out.println("removed ele: " + st.pop());
+        System.out.println("removed ele: " + st.pop());
+        System.out.println("size: " + st.size());
+        System.out.println("removed ele: " + st.pop());
+
+        // System.out.println("removed ele: " + st.pop());
+
+        // experiment
+        Stack st2 = new Stack<>();
+        
+        st2.push(10);
+        st2.push("ritik");
+
+        System.out.println(st2.size());
+
+        System.out.println(st2.pop());
+        System.out.println(st2.pop());
+    }
+}
---
+++ M3 Jan/Stacks/Stacks 1/MyStack/Main.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class Main {
+    static class MyStack {
+        ArrayList<Integer> list;
+
+        MyStack() {
+            list = new ArrayList<>();
+        }
+
+        public void push(int e) {
+            list.add(e);
+        }
+
+        public int peek() {
+            if (list.size() == 0) {
+                System.out.println("Underflow");
+                return -1;
+            } else {
+                int size = list.size();
+                return list.get(size - 1);
+            }
+        }
+
+        public int pop() {
+            if (list.size() == 0) {
+                System.out.println("Underflow");
+                return -1;
+            } else {
+                int size = list.size();
+                return list.remove(size - 1);
+            }
+        }
+
+        public int size() {
+            return list.size();
+        }
+    }
+    
+    public static void main(String[] args) {
+        MyStack st = new MyStack();
+
+        // push method: Pushes an item onto the top of this stack.
+        st.push(10);
+        st.push(30);
+        st.push(40);
+        st.push(5);
+
+        // get topmost element 
+        System.out.println("topmost element: " + st.peek());
+
+        // size of the stacj
+        System.out.println("size: " + st.size());
+
+        // remove from stack
+        System.out.println("removed ele: " + st.pop());
+        System.out.println("removed ele: " + st.pop());
+        System.out.println("removed ele: " + st.pop());
+        System.out.println("size: " + st.size());
+        System.out.println("removed ele: " + st.pop());
+
+        System.out.println("removed ele: " + st.pop());
+    }
+}
